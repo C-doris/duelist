1e3e283a5a42c4abe480838b8c4f7734b2dd698c
Doris Obodoefuna <70792338+C-doris@users.noreply.github.com>
2026-02-15 16:45:52 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'assignment-tracker-v6';
+const CACHE_NAME = 'assignment-tracker-v7';
 const FILES = [
   './',
   './index.html',
@@ -32,6 +32,7 @@ self.addEventListener('fetch', (e) => {
   );
 
 });
+
 
 
 
